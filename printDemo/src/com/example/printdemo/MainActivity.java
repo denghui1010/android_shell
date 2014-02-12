209f505bc2d5667517da996a8f3fd9654238e6be
liudenghui <liudenghui@goodow.com>
2014-02-12 18:41:15 +0800

    init

---
+++ printDemo/src/com/example/printdemo/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.printdemo;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void test(View view) {
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          Socket socket = new Socket();
+          socket.connect(new InetSocketAddress("192.168.9.14", 9100), 3000);
+          // PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+          // printWriter.write("测试");
+          // printWriter.write("abc");
+          // printWriter.write("!@#");
+          // printWriter.write("123");
+          // printWriter.close();
+          FileInputStream fis = new FileInputStream("sdcard/1.jpg");
+          PrintStream printStream = new PrintStream(socket.getOutputStream());
+          byte[] buf = new byte[1024];
+          int len = 0;
+          while ((len = fis.read()) != -1) {
+            printStream.write(buf, 0, len);
+          }
+          fis.close();
+          printStream.close();
+          socket.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }).start();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+}
